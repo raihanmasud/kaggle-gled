6ab050bce2d23d744dd4580f51758f527bdc9b6d
raihanmasud <raihan.masud@gmail.com>
2015-08-30 17:37:39 -0700

    re-structured for all the subjects

--- gled.py
+++ gled.py
@@ -68,92 +68,7 @@ cols = ['HandStart', 'FirstDigitTouch',
 subjects = range(1, 13)
 ids_tot = []
 pred_tot = []
-
-
-###loop on subjects and 8 series for train data + 2 series for test data
-for subject in subjects:
-    y_raw = []
-    raw = []
-    ##debug code remove
-    if subject > 1:
-        continue
-
-    # ################ READ DATA ################################################
-    fnames = glob('../train1/subj%d_series*_data.csv' % (subject))
-
-    for fname in fnames:
-        data, labels = prepare_data_train(fname)
-        raw.append(data)
-        y_raw.append(labels)
-
-    X = pd.concat(raw)
-    y = pd.concat(y_raw)
-
-    # transform in numpy array
-    # transform train data in numpy array
-    X_train = np.asarray(X.astype(np.float32))
-    y = np.asarray(y.astype(np.float32))
-
-################ Read test data #####################################
-
-    fnames = glob('../test1/subj%d_series*_data.csv' % (subject))
-
-    test = []
-    idx = []
-    for fname in fnames:
-        data = prepare_data_test(fname)
-        test.append(data)
-        idx.append(np.array(data['id']))
-
-    X_test = pd.concat(test)
-    ids = np.concatenate(idx)
-    ids_tot.append(ids)
-    X_test = X_test.drop(['id'], axis=1)  # remove id
-    # transform test data in numpy array
-    X_test = np.asarray(X_test.astype(np.float32))
-
-
-####process training data####
-X = X_train
-X = data_preprocess_train(X)
-NO_TIME_POINTS = 100
-TOTAL_TIME_POINTS = len(X) // NO_TIME_POINTS
-
-no_rows = TOTAL_TIME_POINTS * NO_TIME_POINTS
-X = X[0:no_rows, :]
-print('X ', X.shape)
-
-X = X.transpose()
-X_Samples = np.split(X, TOTAL_TIME_POINTS, axis=1)
-X = np.asarray(X_Samples)
-print('X({0})'.format(X.shape))
-
-y = y[0:no_rows, :]
-y = y[::NO_TIME_POINTS, :]
-print('y({0})'.format(y.shape))
-
-####process test data####
-X_test = X_test
-X_test = data_preprocess_test(X_test)
-NO_TIME_POINTS = 100
-TOTAL_TIME_POINTS = len(X_test) // NO_TIME_POINTS
-REMAINDER_TEST_POINTS = len(X_test) % NO_TIME_POINTS
-
-no_rows = TOTAL_TIME_POINTS * NO_TIME_POINTS
-X_test = X_test[0:no_rows, :]
-print('X_test ', X_test.shape)
-
-X_test = X_test.transpose()
-X_test_Samples = np.split(X_test, TOTAL_TIME_POINTS, axis=1)
-X_test = np.asarray(X_test_Samples)
-print('X_test({0})'.format(X_test.shape))
-
-
-#ids_tot[0] = ids_tot[0][:no_rows]
-#ids_tot[0] = ids_tot[0][::NO_TIME_POINTS]
-#print('id_test {}'.format(len(ids_tot[0])))
-
-###########################################################################
+test_dict = dict()
 
 def float32(k):
     return np.cast['float32'](k)
@@ -164,11 +79,10 @@ batch_size = None  #None = arbitary batch size
 hidden_layer_size = 100  #change to 1024
 N_EVENTS = 6
 max_epochs = 5  #increase it
-
+NO_TIME_POINTS = 100
 
 def loss(x, t):
     return aggregate(binary_crossentropy(x, t))
-
 
 net = NeuralNet(
     layers=[
@@ -192,8 +106,8 @@ net = NeuralNet(
     dropout3_p=0.5, hidden5_num_units=hidden_layer_size,
     dropout4_p=0.5, output_num_units=N_EVENTS, output_nonlinearity=sigmoid,
 
-    batch_iterator_train = BatchIterator(batch_size=20),
-    batch_iterator_test = BatchIterator(batch_size=20),
+    batch_iterator_train = BatchIterator(batch_size=200),
+    batch_iterator_test = BatchIterator(batch_size=200),
 
     y_tensor_type=theano.tensor.matrix,
     update=nesterov_momentum,
@@ -207,23 +121,119 @@ net = NeuralNet(
     verbose=1,
 )
 
-net.fit(X, y)
 
-params = net.get_all_params_values()
-learned_weights = net.load_params_from(params)
-probabilities = net.predict_proba(X_test)
 
-for i, p in enumerate(probabilities):
-    print("subj{0}_series{1}_{2}: {3}".format(1,9,i, p)) #Todo: update for all subjects
-    for j in range(NO_TIME_POINTS):
-        pred_tot.append(p)
+###loop on subjects and 8 series for train data + 2 series for test data
+for subject in subjects:
+    y_raw = []
+    raw = []
+    ##debug code remove
+    #if subject > 1:
+    #    continue
 
-for k in range(REMAINDER_TEST_POINTS):
-    pred_tot.append(pred_tot[-1])
+    # ################ READ DATA ################################################
+    fnames = glob('../train1/subj%d_series*_data.csv' % (subject))
+
+    for fname in fnames:
+        data, labels = prepare_data_train(fname)
+        raw.append(data)
+        y_raw.append(labels)
+
+    if raw and y_raw:
+        X = pd.concat(raw)
+        y = pd.concat(y_raw)
+
+    # transform in numpy array
+    # transform train data in numpy array
+        X_train = np.asarray(X.astype(np.float32))
+        y = np.asarray(y.astype(np.float32))
+
+####process training data####
+    X = X_train
+    X = data_preprocess_train(X)
+    total_time_points = len(X) // NO_TIME_POINTS
+
+    no_rows = total_time_points * NO_TIME_POINTS
+    X = X[0:no_rows, :]
+    print('X ', X.shape)
+
+    X = X.transpose()
+    X_Samples = np.split(X, total_time_points, axis=1)
+    X = np.asarray(X_Samples)
+    print('X({0})'.format(X.shape))
+
+    y = y[0:no_rows, :]
+    y = y[::NO_TIME_POINTS, :]
+    print('y({0})'.format(y.shape))
+
+    print("Training subject%d...." %(subject))
+
+    #Todo:Uncomment this line ot train model
+    net.fit(X,y)
+
+################ Read test data #####################################
+
+    fnames = glob('../test/subj%d_series*_data.csv' % (subject))
+
+    test = []
+    idx = []
+    print(type(fnames))
+    fnames.reverse()
+    print(fnames)
+    for fname in fnames:
+        data = prepare_data_test(fname)
+        test.append(data)
+        idx.append(np.array(data['id']))
+
+        data_size = len(data)
+        series = 9 if 'series9' in fname else 10
+        data_name = 'subj{0}_series{1}'.format(subject, series)
+        test_dict[data_name] = data_size
+
+    X_test = pd.concat(test)
+    ids = np.concatenate(idx)
+    ids_tot.append(ids)
+    X_test = X_test.drop(['id'], axis=1)  # remove id
+    # transform test data in numpy array
+    X_test = np.asarray(X_test.astype(np.float32))
+
+
+####process test data####
+    X_test = X_test
+    X_test = data_preprocess_test(X_test)
+    total_test_time_points = len(X_test) // NO_TIME_POINTS
+    remainder_test_points = len(X_test) % NO_TIME_POINTS
+
+    no_rows = total_test_time_points * NO_TIME_POINTS
+    X_test = X_test[0:no_rows, :]
+    print('X_test ', X_test.shape)
+
+    X_test = X_test.transpose()
+    X_test_Samples = np.split(X_test, total_test_time_points, axis=1)
+    X_test = np.asarray(X_test_Samples)
+    print('X_test({0})'.format(X_test.shape))
+
+
+###########################################################################
+
+    params = net.get_all_params_values()
+    learned_weights = net.load_params_from(params)
+    probabilities = net.predict_proba(X_test)
+
+    for series in [9,10]:
+        for i, p in enumerate(probabilities):
+            print("subj{0}_series{1}_{2}: {3}".format(subject, series, i, p))
+            for j in range(NO_TIME_POINTS):
+                pred_tot.append(p)
+        sub = 'sub{0}_series{1}'.format(subject, series)
+        data_len = test_dict[sub]
+        remainder_data = data_len % NO_TIME_POINTS
+        for k in range(remainder_data):
+            pred_tot.append(pred_tot[-1])
 
 
 # submission file
-submission_file = 'gled_conv_net_grasp.csv'
+submission_file = './gled_conv_net_grasp.csv'
 # # create pandas object for sbmission
 
 submission = pd.DataFrame(index=np.concatenate(ids_tot),
@@ -232,19 +242,3 @@ submission = pd.DataFrame(index=np.concatenate(ids_tot),
 # # write file
 submission.to_csv(submission_file, index_label='id', float_format='%.6f')
 # submission file
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
